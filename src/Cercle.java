45d5d4ca3d050570122deec3371849179ce03bf1
kaz <fahirah.diarra@gmail.com>
2020-04-09 15:48:01 -0400

    init commit

---
+++ src/Cercle.java
@@ -0,0 +1,11 @@
+public class Cercle extends Forme {
+
+    int rayon;
+
+    public Cercle(char caractere, int x, int y, int rayon) {
+        this.rayon = rayon;
+        this.x = x;
+        this.y = y;
+    }
+
+}
---
+++ src/Rectangle.java
@@ -0,0 +1,23 @@
+public class Rectangle extends Forme {
+
+    int hauteur;
+    int largeur;
+
+    public Rectangle(char caractere, int x, int y, int largeur, int hauteur) {
+        this.hauteur = hauteur;
+        this.largeur = largeur;
+        this.x = x;
+        this.y = y;
+    }
+
+
+    /**
+     * Permet de dessiner le rectangle sur la surface
+     *
+     * @param
+     */
+    public void dessiner() {
+
+    }
+
+}
