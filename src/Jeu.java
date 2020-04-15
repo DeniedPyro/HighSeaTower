91cc09f2fe8d22affad1e0c69d501c48913a61df
kaz <fahirah.diarra@gmail.com>
2020-04-14 22:50:33 -0400

    added fix

--- src/Jeu.java
+++ src/Jeu.java
@@ -27,34 +27,34 @@ public class Jeu {
             platforms.add(generatePlatform(370 - i * 110));
         }
 
-        medusa = new Medusa(WIDTH/2-25, HEIGHT-50);
+        medusa = new Medusa(WIDTH / 2 - 25, HEIGHT - 50);
     }
 
-    
-    /** 
+
+    /**
      * @return boolean
      */
-    public boolean getDebug(){
+    public boolean getDebug() {
         return this.debug;
     }
 
-    
-    /** 
+
+    /**
      * @param debug
      */
-    public void setDebug(boolean debug){
+    public void setDebug(boolean debug) {
         this.debug = debug;
     }
 
-    
-    /** 
+
+    /**
      * @return Medusa
      */
     public Medusa getMedusa() {
         return medusa;
     }
-    
-    /** 
+
+    /**
      * @param min
      * @param max
      * @return int
@@ -63,8 +63,8 @@ public class Jeu {
         return R.nextInt((max - min) + 1) + min;
     }
 
-    
-    /** 
+
+    /**
      * @param b
      */
     private void addBubbleGroup(ArrayList<Bubble> b) {
@@ -73,50 +73,42 @@ public class Jeu {
         for (int i = 0; i < 5; i++) {
             int v = generateNumBetween(350, 450);
             int r = generateNumBetween(5, 20);
-            int x = generateNumBetween(0, 40)-20;
+            int x = generateNumBetween(0, 40) - 20;
             int y = generateNumBetween(0, 20);
             b.add(new Bubble(basex - x, HEIGHT - y + windowY, r, -v));
         }
     }
 
-        
-        /** 
-         * @return boolean
-         */
-        private boolean isMedusaUp75(){
+
+    /**
+     * @return boolean
+     */
+    private boolean isMedusaUp75() {
         return (medusa.y < HEIGHT * 0.25 + this.windowY);
     }
 
-    
-    /** 
+
+    /**
      * @param y
      * @return Platform
      */
-    private Platform generatePlatform(int y){
+    private Platform generatePlatform(int y) {
         int width = R.nextInt(96) + 80;
         int x = R.nextInt(WIDTH - width + 1);
 
-        int choice = R.nextInt(100)+1;
-        //to debug red platforms
-        //if(true) return new PlateformeAccelere(width,x,y);
+        int choice = R.nextInt(100) + 1;
 
-        if (choice <=65){
-            return new PlateformeSimple(width,x,y);
-        }
+        if (choice <= 65) {
+            return new PlateformeSimple(width, x, y);
+        } else if (choice > 65 && choice <= 85) {
+            return new PlateformeRebond(width, x, y);
+        } else if (choice > 85 && choice <= 95) {
+            return new PlateformeAccelere(width, x, y);
 
-        else if (choice > 65 && choice <= 85){
-            return new PlateformeRebond(width,x,y);
-        }
-
-        else if (choice > 85 && choice <= 95){
-            return new PlateformeAccelere(width,x,y);
-
-        }
-        else{
-            if(platforms.size() == 0) {
+        } else {
+            if (platforms.size() == 0) {
                 return new PlateformeSolide(width, x, y);
-            }
-            else {
+            } else {
                 Platform lastPlatform = platforms.get(platforms.size() - 1);
 
                 Color color = Color.rgb(184, 15, 36);
@@ -132,36 +124,36 @@ public class Jeu {
         medusa.jump();
     }
 
-    
-    /** 
+
+    /**
      * @return double
      */
-    public double getScreenVy(){
+    public double getScreenVy() {
         return this.screenVy;
     }
 
-    
-    /** 
+
+    /**
      * @param vy
      */
-    public void setScreenVy(double vy){
-        this.screenVy =vy;
+    public void setScreenVy(double vy) {
+        this.screenVy = vy;
     }
 
-    
-    /** 
+
+    /**
      * @return double
      */
-    public double getScreenAy(){
+    public double getScreenAy() {
         return this.screenAy;
     }
 
-    
-    /** 
+
+    /**
      * @param ay
      */
-    public void setScreenAy(double ay){
-        this.screenAy =ay;
+    public void setScreenAy(double ay) {
+        this.screenAy = ay;
     }
 
 
@@ -173,39 +165,38 @@ public class Jeu {
         medusa.moveRight();
     }
 
-    public void stop(){
+    public void stop() {
         medusa.stop();
     }
 
-    
-    /** 
+
+    /**
      * @param dt
      */
     public void update(double dt) {
 
-        this.bubbleTimeIntervalTrack +=dt;
+        this.bubbleTimeIntervalTrack += dt;
 
-        if(this.bubbleTimeIntervalTrack > 3.0 &&  this.bubbles.isEmpty() ){
-            for (int i =0; i < 3; i++){
+        if (this.bubbleTimeIntervalTrack > 3.0 && this.bubbles.isEmpty()) {
+            for (int i = 0; i < 3; i++) {
                 addBubbleGroup(bubbles);
             }
-            this.bubbleTimeIntervalTrack -=3 ;
+            this.bubbleTimeIntervalTrack -= 3;
         }
 
-        if (!this.bubbles.isEmpty()){
+        if (!this.bubbles.isEmpty()) {
             for (int i = 0; i < this.bubbles.size(); i++) {
                 Bubble b = this.bubbles.get(i);
-                //b.updateAcc(b.getX(), b.getY()-HEIGHT-b.getY());
                 b.update(dt);
-
-                // Collision de la balle avec toutes les balles suivantes
-                //for (int j = i + 1; j < bubbles.size(); j++) {
-                //    b.testCollision(this.bubbles.get(j));
-                //}
-        }
+            }
         }
 
-        if (medusa.hasMoved()) {
+        if (medusa.hasMoved() && ! debug) {
+            screenVy += dt * screenAy;
+            windowY -= dt * screenVy;
+        }
+
+        if (isMedusaUp75() && debug){
             screenVy += dt * screenAy;
             windowY -= dt * screenVy;
         }
@@ -215,82 +206,79 @@ public class Jeu {
          * À chaque tour, on recalcule si le personnage se trouve parterre ou
          * non
          */
-        if (platforms.get(platforms.size()-1).y -windowY > 110 && windowY < 0){
-            Platform p = this.generatePlatform((int) platforms.get(platforms.size() - 1).y-110);
+        if (platforms.get(platforms.size() - 1).y - windowY > 110 && windowY < 0) {
+            Platform p = this.generatePlatform((int) platforms.get(platforms.size() - 1).y - 110);
             platforms.add(p);
         }
 
         Iterator<Platform> platformIterator = platforms.iterator();
 
-        while(platformIterator.hasNext()){
+        while (platformIterator.hasNext()) {
             Platform p = platformIterator.next();
-            if (medusa.collides(p)){
-                p.giveEffect(this,medusa);
-                }
-            else {
-                p.cancelEffect(this,medusa);
+            if (medusa.collides(p)) {
+                p.giveEffect(this, medusa);
+            } else {
+                p.cancelEffect(this, medusa);
             }
         }
         medusa.update(dt);
 
-        if(isMedusaUp75() && speedEffect == 0){
+        if (isMedusaUp75() && speedEffect == 0) {
             System.out.println();
             this.speedEffect = this.screenVy;
             this.screenVy = Math.abs(medusa.vy);
             lock = true;
         }
 
-        if(!isMedusaUp75() && lock){
+        if (!isMedusaUp75() && lock) {
             this.screenVy = this.speedEffect;
             this.speedEffect = 0;
             lock = false;
         }
 
-        if (medusa.y > HEIGHT + this.windowY){
-            medusa.isAlive= false;
+        if (medusa.y > HEIGHT + this.windowY) {
+            medusa.isAlive = false;
         }
     }
 
-    
-    /** 
+
+    /**
      * @param context
      */
     public void draw(GraphicsContext context) {
         context.setFill(Color.DARKBLUE);
         context.fillRect(0, 0, WIDTH, HEIGHT);
 
-        medusa.draw(context,windowY);
+        medusa.draw(context, windowY);
         Iterator<Platform> p = platforms.iterator();
 
-        while(p.hasNext()) {
+        while (p.hasNext()) {
             Platform obj = p.next();
-            if (-obj.y > -Jeu.windowY-HEIGHT){
+            if (-obj.y > -Jeu.windowY - HEIGHT) {
                 obj.draw(context, Jeu.windowY);
-            }
-            else {
-                context.clearRect(obj.x,obj.y,obj.largeur,obj.hauteur);
+            } else {
+                context.clearRect(obj.x, obj.y, obj.largeur, obj.hauteur);
                 p.remove();
             }
         }
 
-        for(int i=0; i<this.bubbles.size(); i++) {
+        for (int i = 0; i < this.bubbles.size(); i++) {
             Bubble bubble = this.bubbles.get(i);
             if (bubble.getY() > Jeu.windowY - HEIGHT) {
                 bubble.draw(context, Jeu.windowY);
-            }
-            else {
-                context.clearRect(bubble.getX(),bubble.getY(),bubble.getW(),bubble.getH());
+            } else {
+                context.clearRect(bubble.getX(), bubble.getY(), bubble.getW(), bubble.getH());
                 this.bubbles.remove(i); // Retire l'élément zéro
             }
         }
     }
 
-    public void resetJeu(){
+    public void resetJeu() {
         platforms.clear();
         for (int i = 0; i < 4; i++) {
             platforms.add(generatePlatform(370 - i * 110));
         }
-        medusa = new Medusa(WIDTH/2-25, HEIGHT-50);
+        medusa = new Medusa(WIDTH / 2 - 25, HEIGHT - 50);
         screenAy = 2;
         screenVy = 50;
         windowY = 0.0;
